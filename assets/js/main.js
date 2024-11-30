b3419625014d69d52765ada5878fcf4a221d32eb
tindolt <122586790+tindolt@users.noreply.github.com>
2024-11-30 13:43:24 -0700

    Removed Caroseul as it kept breaking. Will add back later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,8 @@
+// Smooth Scroll for CTA button
 document.querySelector('.cta-button').addEventListener('click', function(e) {
   e.preventDefault();
   window.scrollTo({
     top: document.querySelector('.features').offsetTop,
     behavior: 'smooth'
   });
-});
-
-let currentSlide = 0;
-
-function moveSlide(step) {
-  const slides = document.querySelectorAll('.carousel-slide');
-  const totalSlides = slides.length;
-  currentSlide += step;
-  if (currentSlide < 0) {
-    currentSlide = totalSlides - 1;
-  }
-  else if (currentSlide >= totalSlides) {
-    currentSlide = 0;
-  }
-  const offset = -currentSlide * 100;
-  document.querySelector('.carousel-container').style.transform = `translateX(${offset}%)`;
-}
-
-const darkModeButton = document.querySelector('.dark-mode-toggle');
-
-darkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
 });
